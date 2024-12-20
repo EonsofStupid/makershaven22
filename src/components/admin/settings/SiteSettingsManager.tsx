4e38293c325a7af076736d59b809ab77ac25b461
EonsofStupid <101021880+EonsofStupid@users.noreply.github.com>
2024-12-20 09:52:24 -0500

    Update SiteSettingsManager.tsx

--- src/components/admin/settings/SiteSettingsManager.tsx
+++ src/components/admin/settings/SiteSettingsManager.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { supabase } from "../../../integrations/supabase/client";
-import { SiteSettings } from "../../../types/shared";
+import { SiteSettings } from "@/lib/types/shared/shared";
 
 toast.configure();
 
